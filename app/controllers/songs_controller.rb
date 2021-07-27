3637a687b55b72fc792de9bc183a2eb89832122f
bcharlotin1 <biancacharlotin1@gmail.com>
2021-07-27 17:12:29 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,16 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      #params[:author_id] coming from? Rails provides it for us through the nested route,
-      #The conditional hinges on whether there's an :author_id key in the params hash — in other words, whether the user navigated to /authors/:id/posts 
-      @song = Artist.find(params[:artist_id]).songs
-      #accccounts for the new nest urls, so see if the user is tryting to access all pots or all post form a particular author
-      if !@song
-        redirect_to artists_path
-      end
+          #params[:author_id] coming from? Rails provides it for us through the nested route,
+          #The conditional hinges on whether there's an :author_id key in the params hash — in other words, whether the user navigated to /authors/:id/posts 
+          @artist = Artist.find_by_id(params[:artist_id])
+          #accccounts for the new nest urls, so see if the user is tryting to access all pots or all post form a particular author
+          if !@artist
+            flash[:alert] = "Artist not found"
+            redirect_to artists_path
+          else  
+            @songs = @artist.songs
+          end
     else
       @songs = Song.all
     
@@ -15,10 +18,13 @@ class SongsController < ApplicationController
   end
 
   def show
+
     @song = Song.find(params[:id])
     if @song
+
     else
-      redirect_to artist_songs_path(params[:artist_id])
+      flash[:alert] = "Song not found"
+      redirect_to artist_songs_path(@song.artist)
     end
   end
 
