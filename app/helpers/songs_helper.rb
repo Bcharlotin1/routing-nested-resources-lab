bf3aa79fdd5374abc4fb60ee7aae9d0552555863
bcharlotin1 <biancacharlotin1@gmail.com>
2021-07-28 19:36:10 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,5 @@
 module SongsHelper
-
+    def display_song(song, artist)
+        song.nil? ? link_to("Add Artist", artist_songs_path(artist)) : link_to(song.artist_name, artist_path(song.artist))
+      end
 end
